ae5f22d3a2bbe5727f5221f16e898838e6892a62
Ben Bertrands <ben.bertrands@gmail.com>
2017-09-23 17:45:56 +0200

    benb: extended wip mappingbuilder

--- index.ts
+++ index.ts
@@ -1,17 +1,95 @@
-import * as child from 'child-process-es6-promise';
-
 export class WiremockClient {
-    stubFor = (mappingBuilder: MappingBuilder):StubMapping => {
-        return new StubMapping();
+    stubFor = (mappingBuilder: BasicMappingBuilder): StubMapping => {
+        return this.givenThat(mappingBuilder);
     }
 
-    givenThat = (mappingBuilder: MappingBuilder): StubMapping => {
+    givenThat = (mappingBuilder: BasicMappingBuilder): StubMapping => {
         return new WiremockClient();
     }
 }
 
-export class MappingBuilder {
+export class BasicMappingBuilder {
+    private requestPatternBuilder: RequestPatternBuilder;
+    private responseDefBuilder: ResponseDefinitionBuilder;
+    private priority: number;
+    private scenarioName: string;
+    private requiredScenarioState: string;
+    private newScenarioState: string;
+    private id: string = uuidv4();
+    private name: string;
+    private isPersistent: boolean = false;
+    private postServeActions: any = {};
 
+    constructor(method: RequestMethod, urlPattern: UrlPattern) {
+        this.requestPatternBuilder = new RequestPatternBuilder(method, urlPattern);
+    }
+
+    uuidv4() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+
+    willReturn(responseDefBuilder: ResponseDefinitionBuilder) {
+        this.responseDefBuilder = responseDefBuilder;
+        return this;
+    }
+
+    withQueryParam(key: string, queryParamPattern: string): BasicMappingBuilder {
+        this.requestPatternBuilder.withQueryParam(key, queryParamPattern);
+        return this;
+    }
+
+    withRequestBody(bodyPattern: ContentPattern): BasicMappingBuilder {
+        this.requestPatternBuilder.withRequestBody(bodyPattern);
+        return this;
+    }
+
+
+}
+
+export class RequestPatternBuilder {
+    private url: UrlPattern;
+    private method: RequestMethod;
+    // private headers: any = {};
+    // private queryParams: any = {};
+    // private bodyPatterns: any = [];
+    // private cookies: any = {};
+    // private basicCredentials: BasicCredentials;
+
+
+    constructor(url: UrlPattern, method: RequestMethod) {
+        this.url = url;
+        this.method = method;
+    }
+}
+
+export class RequestMethod {
+    public static GET: RequestMethod = new RequestMethod("GET");
+    public static POST: RequestMethod = new RequestMethod("POST");
+    public static PUT: RequestMethod = new RequestMethod("PUT");
+    public static DELETE: RequestMethod = new RequestMethod("DELETE");
+    public static PATCH: RequestMethod = new RequestMethod("PATCH");
+    public static OPTIONS: RequestMethod = new RequestMethod("OPTIONS");
+    public static HEAD: RequestMethod = new RequestMethod("HEAD");
+    public static TRACE: RequestMethod = new RequestMethod("TRACE");
+    public static ANY: RequestMethod = new RequestMethod("ANY");
+
+    private name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+export class UrlPattern {
+    private pattern: string;
+
+
+    constructor(pattern: string) {
+        this.pattern = pattern;
+    }
 }
 
 export class StubMapping {
